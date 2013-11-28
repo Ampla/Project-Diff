cb76a22b251563a047598e34d78aff8e27377bbf
Joseph Plant <git@JosephPlant.com>
2013-11-28 13:48:48 +1000

    Basic CMD script

---
+++ src/DiffProject.cmd
@@ -0,0 +1,43 @@
+rem @echo off
+
+set project=AmplaProject.xml
+set authstore=AuthStore.xml
+
+set production=..\Production\
+set development=..\Development\
+
+if EXIST Working goto Working_exists
+mkdir Working
+:Working_exists
+
+if NOT EXIST Working\Production goto working_production_create
+rmdir Working\Production /S /Q
+
+:Working_production_create
+mkdir Working\Production
+
+if NOT EXIST Working\Development goto working_development_create
+rmdir Working\Development /S /Q
+
+:Working_development_create
+mkdir Working\Development
+
+set nxslt=..\lib\nxslt\nxslt.exe
+set graphviz=..\lib\GraphViz-2.30.1\bin
+set dotml=..\lib\dotml-1.4
+set xsltproc=..\lib\libxml\bin\xsltproc.exe 
+
+set directory=%production%
+set type=Production
+
+@echo === Normalise ===
+%nxslt% %directory%%project% StyleSheets\Project.Normalize.xslt -o Working\%type%\project.xml 
+%xsltproc% -o Working\%type%\output.files.xml StyleSheets\Output.Project.Files.xslt Working\%type%\project.xml 
+
+set directory=%development%
+set type=Development
+
+@echo === Normalise ===
+%nxslt% %directory%%project% StyleSheets\Project.Normalize.xslt -o Working\%type%\project.xml 
+%xsltproc% -o Working\%type%\output.files.xml StyleSheets\Output.Project.Files.xslt Working\%type%\project.xml 
+
